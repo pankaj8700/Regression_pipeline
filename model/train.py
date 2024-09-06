28fc8d6556dd79e3eaef48cef3e23b7037cb342d
Pankaj8700 <pankajku.8700@gmail.com>
2024-09-06 19:55:47 +0530

    again

--- model/train.py
+++ model/train.py
@@ -4,12 +4,8 @@ warnings.filterwarnings('ignore')
 
 # Import necessary libraries
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
 
 # Load the dataset
 column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
