561040c1e443ad6db293a9e69eec5eaadea162f9
Pankaj8700 <pankajku.8700@gmail.com>
2024-09-06 20:18:38 +0530

    again_2

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,12 @@
 import unittest
 import joblib
 from sklearn.linear_model import LinearRegression
+
 class TestModelTraining(unittest.TestCase):
- def test_model_training(self):
- model = joblib.load('model/Boston_model.pkl')
- self.assertIsInstance(model, LinearRegression)
- 
-self.assertGreaterEqual(len(model.feature_importances_
-), 4)
+    def test_model_training(self):
+    	model = joblib.load('model/Boston_model.pkl')
+        self.assertIsInstance(model, LinearRegression)
+        self.assertGreaterEqual(len(model.feature_importances_), 4)
+
 if __name__ == '__main__':
- unittest.main()
+    unittest.main()
